df13c601f07ee1706832bf946e855434871b8e52
Conor Pope <conorpope@fouroneone.co.uk>
2026-05-30 22:00:12 +0200

    Add social preview metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,33 @@ import Shell from '@/components/layout/Shell'
 import './globals.css'
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://411sweepstake.com'),
   title: '411 World Cup Sweepstake 2026',
   description:
     'Find a team. Follow them to glory. 411\'s office World Cup sweepstake for the 2026 FIFA World Cup.',
+  openGraph: {
+    title: '411 World Cup Sweepstake 2026',
+    description:
+      'Find a team. Follow them to glory. 411\'s office World Cup sweepstake for the 2026 FIFA World Cup.',
+    url: 'https://411sweepstake.com',
+    siteName: '411 World Cup Sweepstake',
+    images: [
+      {
+        url: '/WC_sweepstake.png',
+        width: 1536,
+        height: 1024,
+        alt: '411 World Cup Sweepstake logo',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '411 World Cup Sweepstake 2026',
+    description:
+      'Find a team. Follow them to glory. 411\'s office World Cup sweepstake for the 2026 FIFA World Cup.',
+    images: ['/WC_sweepstake.png'],
+  },
 }
 
 export default function RootLayout({
